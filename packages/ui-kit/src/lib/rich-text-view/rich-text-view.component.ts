18398495bd53d7127d5107d709df8116d900958b
Kris <kris@theredhead.nl>
2026-04-17 21:38:32 +0200

    added bypassSecurityTrustHtml to ui-rich-text-view

--- packages/ui-kit/src/lib/rich-text-view/rich-text-view.component.ts
+++ packages/ui-kit/src/lib/rich-text-view/rich-text-view.component.ts
@@ -1,10 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
+  inject,
   input,
   model,
 } from "@angular/core";
-import { UISurface } from '@theredhead/lucid-foundation';
+import { DomSanitizer } from "@angular/platform-browser";
+import { UISurface } from "@theredhead/lucid-foundation";
 
 /**
  * A read-only rich text renderer.
@@ -21,7 +24,7 @@ import { UISurface } from '@theredhead/lucid-foundation';
   selector: "ui-rich-text-view",
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
-  hostDirectives: [{ directive: UISurface, inputs: ['surfaceType'] }],
+  hostDirectives: [{ directive: UISurface, inputs: ["surfaceType"] }],
   host: {
     class: "ui-rich-text-view",
   },
@@ -29,9 +32,14 @@ import { UISurface } from '@theredhead/lucid-foundation';
   styleUrl: "./rich-text-view.component.scss",
 })
 export class UIRichTextView {
-
-  /** The HTML content to render. */
+  private readonly sanitizer = inject(DomSanitizer);
+  /** The raw HTML content to render. */
   public readonly content = model<string>("");
+
+  /** The HTML content to render, as trusted */
+  public readonly trustedContent = computed(() => {
+    this.sanitizer.bypassSecurityTrustHtml(this.content());
+  });
 
   /** Accessible label for the container. */
   public readonly ariaLabel = input<string>("Rich text content");
